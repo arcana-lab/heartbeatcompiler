adb8ee0bb16bc04f9d3c96eae6dc629be94f242b
Simone Campanoni <simo.xan@gmail.com>
2021-09-21 16:31:48 -0500

    Example: added a simple C program

---
+++ compiler/example/incr_array.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int heartbeat ;
+
+void incr_array(int* a, int i, int sz) {
+  for (int i = 0; i < sz; i++) {
+    if (heartbeat) {
+      loop_handler(a, i, sz);
+    }
+    a[i]++;
+  }
+}
+
+//#if 0
+int loop_handler(int* a, int i, int sz) {
+  if (sz - i < 2) {
+    return 0; // no promotion
+  }
+  int mid = (i + sz) / 2;
+  // fork in parallel (call tpal runtime)
+  incr_array(a, i, mid); 
+  incr_array(a, mid, sz);
+  // block until both branches finish
+  return 1;
+}
+//#endif
+
+int main() {
+  int sz = 1000;
+  int* a = (int*)malloc(sizeof(int) * sz);
+  for (int i = 0; i < sz; i++) {
+    a[i]++;
+  }  
+  return 0;
+}
