38c801cba6d2aca2ffa3cc21708f78262cdafc31
Simone Campanoni <simo.xan@gmail.com>
2021-10-29 16:37:13 -0500

    Example: extended the code to have inner loop

--- compiler/example/incr_array.cpp
+++ compiler/example/incr_array.cpp
@@ -4,21 +4,24 @@
 #include <iostream>
 #include "loop_handler.cpp"
 
-void HEARTBEAT_loop0 (uint64_t sz, int32_t *a){
+void HEARTBEAT_loop0 (uint64_t sz, int32_t *a, uint32_t innerIterations){
   for (uint64_t i = 0; i < sz; i++) {
-    a[i] = (int32_t)sin((double)a[i]);
+    for (auto j = 0; j < innerIterations; j++){
+      a[i] = (int32_t)sin((double)a[i]);
+    }
   }
 }
 
 int main (int argc, char *argv[]) {
-  if (argc < 2){
-    std::cerr << "USAGE: " << argv[0] << " ITERATIONS" << std::endl;
+  if (argc < 3){
+    std::cerr << "USAGE: " << argv[0] << " OUTER_ITERATIONS INNER_ITERATIONS" << std::endl;
     return 1;
   }
   auto sz = atoll(argv[1]);
+  auto inner = atoi(argv[2]);
   auto a = (int32_t*)calloc(sz, sizeof(int32_t));
   
-  HEARTBEAT_loop0(sz, a);
+  HEARTBEAT_loop0(sz, a, inner);
 
   printf("%lld %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
 
