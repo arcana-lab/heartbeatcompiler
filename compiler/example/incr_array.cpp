1144c9dd91201272dac31b6b7a8dd8f1971f54cf
Simone Campanoni <simo.xan@gmail.com>
2021-10-29 16:40:08 -0500

    Example: extended to parallelize two loops

--- compiler/example/incr_array.cpp
+++ compiler/example/incr_array.cpp
@@ -12,6 +12,14 @@ void HEARTBEAT_loop0 (uint64_t sz, int32_t *a, uint32_t innerIterations){
   }
 }
 
+void HEARTBEAT_loop1 (uint64_t sz, int32_t *a, uint32_t innerIterations){
+  for (uint64_t i = 0; i < sz; i++) {
+    for (auto j = 0; j < innerIterations; j++){
+      a[i]++;
+    }
+  }
+}
+
 int main (int argc, char *argv[]) {
   if (argc < 3){
     std::cerr << "USAGE: " << argv[0] << " OUTER_ITERATIONS INNER_ITERATIONS" << std::endl;
@@ -22,6 +30,8 @@ int main (int argc, char *argv[]) {
   auto a = (int32_t*)calloc(sz, sizeof(int32_t));
   
   HEARTBEAT_loop0(sz, a, inner);
+
+  HEARTBEAT_loop1(sz, a, inner);
 
   printf("%lld %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
 
