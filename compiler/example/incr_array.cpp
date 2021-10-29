8dabf80f37066243a1f32da9cc85e3436dcfdeea
Simone Campanoni <simo.xan@gmail.com>
2021-10-29 15:22:11 -0500

    Example: started porting the example to the new format

--- compiler/example/incr_array.cpp
+++ compiler/example/incr_array.cpp
@@ -1,31 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
 #include "loop_handler.cpp"
 
-int main() {
-  int sz = 100000000;
-  int* a = (int*)calloc(sz, sizeof(int));
-  for (int i = 0; i < sz; i++) {
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
-    a[i] = (int)sin((double)a[i]);
+void HEARTBEAT_loop0 (uint64_t sz, int32_t *a){
+  for (uint64_t i = 0; i < sz; i++) {
+    a[i] = (int32_t)sin((double)a[i]);
   }
-  printf("%d %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
+}
+
+int main (int argc, char *argv[]) {
+  if (argc < 2){
+    std::cerr << "USAGE: " << argv[0] << " ITERATIONS" << std::endl;
+    return 1;
+  }
+  auto sz = atoll(argv[1]);
+  auto a = (int32_t*)calloc(sz, sizeof(int32_t));
+  
+  HEARTBEAT_loop0(sz, a);
+
+  printf("%lld %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
+
   return 0;
 }
