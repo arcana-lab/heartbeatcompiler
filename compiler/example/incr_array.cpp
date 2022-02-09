9646cab46a9a3ba89db1b48c01ab80c39edbe7b0
Mike Rainey <rainey@peroni.cs.northwestern.edu>
2022-02-09 17:26:50 -0600

    got incr_array benchmark to run using condor

--- compiler/example/incr_array.cpp
+++ compiler/example/incr_array.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
+#ifdef ORIGINAL_PROGRAM
+#include <chrono>
+#endif
 #include "loop_handler.cpp"
 
 void HEARTBEAT_loop0 (uint64_t sz, int32_t *a, uint32_t innerIterations){
@@ -28,12 +31,25 @@ int main (int argc, char *argv[]) {
   auto sz = atoll(argv[1]);
   auto inner = atoi(argv[2]);
   auto a = (int32_t*)calloc(sz, sizeof(int32_t));
+
   
-  HEARTBEAT_loop0(sz, a, inner);
+  //HEARTBEAT_loop0(sz, a, inner);
+#ifdef ORIGINAL_PROGRAM
+  using clock = std::chrono::system_clock;
+  using sec = std::chrono::duration<double>;
+  const auto before = clock::now();
+#endif
+
 
   HEARTBEAT_loop1(sz, a, inner);
+  
+#ifdef ORIGINAL_PROGRAM
+  const sec duration = clock::now() - before;
+  printf("[{\"exectime\":  %f}]\n", duration.count());
+#endif
+  
 
-  printf("%lld %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
+  //printf("%lld %d %d %d\n", sz, a[0], a[sz/2], a[sz-1]);
 
   return 0;
 }
