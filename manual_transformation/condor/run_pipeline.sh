1613156bd25ad1cde4fece6ac1af92f5be16d70d
Bangyen Pham <bangyen@northwestern.edu>
2022-10-18 10:20:23 -0500

    added baseline pipeline

--- manual_transformation/condor/run_pipeline.sh
+++ manual_transformation/condor/run_pipeline.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
-# source the default environment
-source /home/yso0488/.bash_profile ;
+export TASKPARTS_CPU_BASE_FREQUENCY_KHZ=240000
+ROOT=/nfs-scratch/bhp1038/heart/manual_transformation
+TXT=$ROOT/condor/progress.txt
 
-# go to the executing folder
-cd /home/yso0488/projects/heartbeatcompiler/manual_transformation/condor ;
+cd $ROOT/benchmarks
+make > $TXT 2>&1
 
-# make the progress file
-PROGRESS="progress.txt" ;
-rm $PROGRESS ;
-touch $PROGRESS ;
+for d in */ ; do
+	if [ $d = "scripts/" ] ; then
+		continue
+	fi
 
-# pipeline starts
-echo "Pipeline starts" >> $PROGRESS ;
+	echo "-----COMPILATION-----" >> $TXT
+	make -C $d >> $TXT
+	
+	BINARY=$ROOT/results/current_machine/binary/$d/baseline
+	mkdir -p $BINARY && cp $d/baseline "$_"
+	
+	(
+		TIME=$ROOT/results/time/$d/baseline
+		PERF=$ROOT/results/LLC-load-misses/$d/baseline
+		
+		rm $TIME/time.txt
+		rm $PERF/perf.txt
+		cd $BINARY
 
+		for n in {1..5} ; do
+			echo "ITERATION $n" >> $TXT
 
-echo "Hello from pipeline" ;
+			mkdir -p $TIME
+			echo "-----$n-----" >> $TIME/time.txt
+			{ time -p taskset -c 0 ./baseline >> $TXT; } 2>> $TIME/time.txt
 
+			mkdir -p $PERF
+			echo "-----$n-----" >> $PERF/perf.txt
+			{ perf stat -e LLC-load-misses ./baseline >> $TXT; } 2>> $PERF/perf.txt
+		done
+	)
+done
 
-# pipeline finishes
-echo "Pipeline finishes" >> $PROGRESS ;
-
-exit 0 ;
+echo "-----EXIT-----" >> $TXT
+exit 0
