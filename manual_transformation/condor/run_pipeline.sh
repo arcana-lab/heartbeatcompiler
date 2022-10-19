46548afd197102f3e9182e50949c5a6d958d972a
Bangyen Pham <bangyen@northwestern.edu>
2022-10-19 18:37:54 -0500

    Revert "added baseline pipeline"
    This reverts commit 1613156bd25ad1cde4fece6ac1af92f5be16d70d.

--- manual_transformation/condor/run_pipeline.sh
+++ manual_transformation/condor/run_pipeline.sh
@@ -1,44 +1,24 @@
 #!/bin/bash
 
-export TASKPARTS_CPU_BASE_FREQUENCY_KHZ=240000
-ROOT=/nfs-scratch/bhp1038/heart/manual_transformation
-TXT=$ROOT/condor/progress.txt
+# source the default environment
+source /home/yso0488/.bash_profile ;
 
-cd $ROOT/benchmarks
-make > $TXT 2>&1
+# go to the executing folder
+cd /home/yso0488/projects/heartbeatcompiler/manual_transformation/condor ;
 
-for d in */ ; do
-	if [ $d = "scripts/" ] ; then
-		continue
-	fi
+# make the progress file
+PROGRESS="progress.txt" ;
+rm $PROGRESS ;
+touch $PROGRESS ;
 
-	echo "-----COMPILATION-----" >> $TXT
-	make -C $d >> $TXT
-	
-	BINARY=$ROOT/results/current_machine/binary/$d/baseline
-	mkdir -p $BINARY && cp $d/baseline "$_"
-	
-	(
-		TIME=$ROOT/results/time/$d/baseline
-		PERF=$ROOT/results/LLC-load-misses/$d/baseline
-		
-		rm $TIME/time.txt
-		rm $PERF/perf.txt
-		cd $BINARY
+# pipeline starts
+echo "Pipeline starts" >> $PROGRESS ;
 
-		for n in {1..5} ; do
-			echo "ITERATION $n" >> $TXT
 
-			mkdir -p $TIME
-			echo "-----$n-----" >> $TIME/time.txt
-			{ time -p taskset -c 0 ./baseline >> $TXT; } 2>> $TIME/time.txt
+echo "Hello from pipeline" ;
 
-			mkdir -p $PERF
-			echo "-----$n-----" >> $PERF/perf.txt
-			{ perf stat -e LLC-load-misses ./baseline >> $TXT; } 2>> $PERF/perf.txt
-		done
-	)
-done
 
-echo "-----EXIT-----" >> $TXT
-exit 0
+# pipeline finishes
+echo "Pipeline finishes" >> $PROGRESS ;
+
+exit 0 ;
