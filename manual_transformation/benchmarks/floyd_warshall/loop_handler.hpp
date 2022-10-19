bf335224d9b3d144730dc5f06c46073ab5b2368e
Yian Su <yiansu2018@u.northwestern.edu>
2022-10-19 11:18:37 -0500

    Benchmarks: optimize to use returnLevel instead of splittingLevel

--- manual_transformation/benchmarks/floyd_warshall/loop_handler.hpp
+++ manual_transformation/benchmarks/floyd_warshall/loop_handler.hpp
@@ -36,36 +36,27 @@ int loop_handler(
   (*me) = false;
 
   // decide the splitting level
-  uint64_t splittingLevel = -1;
   for (uint64_t level = 0; level <= myLevel; level++) {
     if (maxIterations[level] - startIterations[level] >= 3) {
-      splittingLevel = level;
+      returnLevel = level;
       break;
     }
   }
   // no more work to split at any level
-  if (splittingLevel == -1) {
+  if (returnLevel > myLevel) {
     return 0;
-  }
-
-  // set returnLevel variable
-  returnLevel = splittingLevel;
-
-  // determine the splitting point of the remaining iterations
-  uint64_t med;
-  if (splittingLevel == myLevel) {
-    med = (startIterations[splittingLevel] + maxIterations[splittingLevel]) / 2;
-  } else {
-    med = (startIterations[splittingLevel] + 1 + maxIterations[splittingLevel]) / 2;
   }
 
   // allocate the new liveInEnvironments for both tasks
   uint64_t *liveInEnvironmentsFirstHalf[2];
   uint64_t *liveInEnvironmentsSecondHalf[2];
-  for (uint64_t level = 0; level <= splittingLevel; level++) {
+  for (uint64_t level = 0; level <= returnLevel; level++) {
     liveInEnvironmentsFirstHalf[level] = ((uint64_t **)liveInEnvironments)[level];
     liveInEnvironmentsSecondHalf[level] = ((uint64_t **)liveInEnvironments)[level];
   }
+
+  // determine the splitting point of the remaining iterations
+  uint64_t med = (startIterations[returnLevel] + 1 + maxIterations[returnLevel]) / 2;
 
   // allocate startIterations and maxIterations for both tasks
   uint64_t startIterationsFirstHalf[2] =  { startIterations[0], startIterations[1]  };
@@ -73,40 +64,40 @@ int loop_handler(
   uint64_t startIterationsSecondHalf[2] = { startIterations[0], startIterations[1]  };
   uint64_t maxIterationsSecondHalf[2] =   { maxIterations[0],   maxIterations[1]    };
 
-  if (splittingLevel != myLevel) {
-    startIterationsFirstHalf[splittingLevel]++;
-  }
-  maxIterationsFirstHalf[splittingLevel] = med;
-  startIterationsSecondHalf[splittingLevel] = med;
+  // set startIterations and maxIterations for both tasks
+  startIterationsFirstHalf[returnLevel]++;
+  maxIterationsFirstHalf[returnLevel] = med;
+  startIterationsSecondHalf[returnLevel] = med;
 
 #if defined(DEBUG_LOOP_HANDLER)
   printf("Loop_handler: Start\n");
   printf("Loop_handler:   Promotion\n");
   printf("Loop_handler:   Receiving Level = %lu\n", myLevel);
-  printf("Loop_handler:   Splitting Level = %lu\n", splittingLevel);
-  printf("Loop_handler:     startIteration = %lu, maxIterations = %lu\n", startIterations[splittingLevel], maxIterations[splittingLevel]);
+  printf("Loop_handler:   Splitting Level = %lu\n", returnLevel);
+  printf("Loop_handler:     startIteration = %lu, maxIterations = %lu\n", startIterations[returnLevel], maxIterations[returnLevel]);
   printf("Loop_handler:     med = %lu\n", med);
-  printf("Loop_handler:     task1: startIteration = %lu, maxIterations = %lu\n", startIterationsFirstHalf[splittingLevel], maxIterationsFirstHalf[splittingLevel]);
-  printf("Loop_handler:     task2: startIteration = %lu, maxIterations = %lu\n", startIterationsSecondHalf[splittingLevel], maxIterationsSecondHalf[splittingLevel]);
+  printf("Loop_handler:     task1: startIteration = %lu, maxIterations = %lu\n", startIterationsFirstHalf[returnLevel], maxIterationsFirstHalf[returnLevel]);
+  printf("Loop_handler:     task2: startIteration = %lu, maxIterations = %lu\n", startIterationsSecondHalf[returnLevel], maxIterationsSecondHalf[returnLevel]);
 #endif
 
-  maxIterations[splittingLevel] = startIterations[splittingLevel] + 1;
+  // reset maxIterations for the leftover task
+  maxIterations[returnLevel] = startIterations[returnLevel] + 1;
 
   /*
     assert(fut == nullptr && "Assigning to a future pointer which already bounded\n");
     fut = new lazy_future([=] {
       taskparts::tpalrts_promote_via_nativefj([&] {
-        (*f[splittingLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, splittingLevel, nullptr);
+        (*f[returnLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, returnLevel, nullptr);
       }, [&] {
-        (*f[splittingLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, splittingLevel, nullptr);
+        (*f[returnLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, returnLevel, nullptr);
       }, [] { }, taskparts::bench_scheduler());
     }
   */
 
   taskparts::tpalrts_promote_via_nativefj([&] {
-    (*f[splittingLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, splittingLevel/*, nullptr */);
+    (*f[returnLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, returnLevel/*, nullptr */);
   }, [&] {
-    (*f[splittingLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, splittingLevel/*, nullptr */);
+    (*f[returnLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, returnLevel/*, nullptr */);
   }, [] { }, taskparts::bench_scheduler());
 
   return 1;
