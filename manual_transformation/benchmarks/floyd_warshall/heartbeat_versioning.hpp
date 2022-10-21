c74d55bb4ac8a898288451a17746b9db39f16404
Yian Su <yiansu2018@u.northwestern.edu>
2022-10-21 00:24:16 -0500

    Benchmarks: code versioning manual transformation for floyd_warshall

--- manual_transformation/benchmarks/floyd_warshall/heartbeat_versioning.hpp
+++ manual_transformation/benchmarks/floyd_warshall/heartbeat_versioning.hpp
@@ -6,4 +6,150 @@
 
 namespace floyd_warshall {
 
+void floyd_warshall_heartbeat_versioning(int *, int);
+void HEARTBEAT_loop0(int *, int, int);
+void HEARTBEAT_loop1(int *, int, int, int);
+void HEARTBEAT_loop0_cloned(uint64_t *, uint64_t *, void **, uint64_t);
+void HEARTBEAT_loop1_cloned(uint64_t *, uint64_t *, void **, uint64_t);
+void HEARTBEAT_loop1_leftover(uint64_t *, uint64_t *, void **, uint64_t);
+
+typedef void(*functionPointer)(uint64_t *, uint64_t *, void **, uint64_t);
+functionPointer splittingTasks[2] = {
+  &HEARTBEAT_loop0_cloned,
+  &HEARTBEAT_loop1_cloned
+};
+functionPointer leftoverTasks[1] = {
+  &HEARTBEAT_loop1_leftover
+};
+
+static bool run_heartbeat = true;
+
+// Entry function for the benchmark
+void floyd_warshall_heartbeat_versioning(int *dist, int vertices) {
+  for (int via = 0; via < vertices; via++) {
+    HEARTBEAT_loop0(dist, vertices, via);
+  }
+}
+
+// Outlined loops
+void HEARTBEAT_loop0(int *dist, int vertices, int via) {
+  if (run_heartbeat) {
+    run_heartbeat = false;
+
+    // allocate live-in environment for loop0
+    uint64_t *liveInEnvironments[2];
+    uint64_t liveInEnvironmentForLoop0[3];
+    liveInEnvironments[0] = (uint64_t *)liveInEnvironmentForLoop0;
+
+    // store into live-in environment
+    liveInEnvironmentForLoop0[0] = (uint64_t)dist;
+    liveInEnvironmentForLoop0[1] = (uint64_t)vertices;
+    liveInEnvironmentForLoop0[2] = (uint64_t)via;
+
+    // allocate the startIterations and maxIterations array
+    uint64_t startIterations[2];
+    uint64_t maxIterations[2];
+    
+    // set the start and max iteration for loop0
+    startIterations[0] = 0;
+    maxIterations[0] = vertices;
+
+    // invoke loop0 in heartbeat form
+    HEARTBEAT_loop0_cloned(startIterations, maxIterations, (void **)liveInEnvironments, 0);
+
+    run_heartbeat = true;
+  } else {
+    for (int from = 0; from < vertices; from++) {
+      HEARTBEAT_loop1(dist, vertices, via, from);
+    }
+  }
+
+  return;
+}
+
+void HEARTBEAT_loop1(int *dist, int vertices, int via, int from) {
+  for (int to = 0; to < vertices; to++) {
+    if ((from != to) && (from != via) && (to != via)) {
+      SUB(dist, vertices, from, to) =
+        std::min(SUB(dist, vertices, from, to),
+                SUB(dist, vertices, from, via) + SUB(dist, vertices, via, to));
+    }
+  }
+
+  return;
+}
+
+// Cloned loops
+void HEARTBEAT_loop0_cloned(uint64_t *startIterations, uint64_t *maxIterations, void **liveInEnvironments, uint64_t myLevel) {
+  // load live-in environment
+  uint64_t *liveInEnvironment = ((uint64_t **)liveInEnvironments)[myLevel];
+  int *dist = (int *)liveInEnvironment[0];
+  int vertices = (int)liveInEnvironment[1];
+  int via = (int)liveInEnvironment[2];
+
+  // allocate live-in environment for loop1
+  uint64_t liveInEnvironmentForLoop1[4];
+  ((uint64_t **)liveInEnvironments)[myLevel + 1] = (uint64_t *)liveInEnvironmentForLoop1;
+
+  // store into loop1's live-in environment
+  liveInEnvironmentForLoop1[0] = (uint64_t)dist;
+  liveInEnvironmentForLoop1[1] = (uint64_t)vertices;
+  liveInEnvironmentForLoop1[2] = (uint64_t)via;
+
+  for (; startIterations[myLevel] < maxIterations[myLevel]; startIterations[myLevel]++) {
+    loop_handler(startIterations, maxIterations, (void **)liveInEnvironments, myLevel, splittingTasks, leftoverTasks);
+
+    // store into loop1's live-in environment
+    liveInEnvironmentForLoop1[3] = (uint64_t)startIterations[myLevel];  // from
+    
+    // set the start and max iteation for loop1
+    startIterations[myLevel + 1] = 0;
+    maxIterations[myLevel + 1] = vertices;
+
+    HEARTBEAT_loop1_cloned(startIterations, maxIterations, liveInEnvironments, myLevel + 1);
+  }
+
+  return;
+}
+
+void HEARTBEAT_loop1_cloned(uint64_t *startIterations, uint64_t *maxIterations, void **liveInEnvironments, uint64_t myLevel) {
+  // load live-in environment
+  uint64_t *liveInEnvironment = ((uint64_t **)liveInEnvironments)[myLevel];
+  int *dist = (int *)liveInEnvironment[0];
+  int vertices = (int)liveInEnvironment[1];
+  int via = (int)liveInEnvironment[2];
+  int from = (int)liveInEnvironment[3];
+
+  for (; startIterations[myLevel] < maxIterations[myLevel]; startIterations[myLevel]++) {
+    loop_handler(startIterations, maxIterations, (void **)liveInEnvironments, myLevel, splittingTasks, leftoverTasks);
+
+    if ((from != startIterations[myLevel]) && (from != via) && (startIterations[myLevel] != via)) {
+      SUB(dist, vertices, from, startIterations[myLevel]) =
+        std::min(SUB(dist, vertices, from, startIterations[myLevel]),
+                SUB(dist, vertices, from, via) + SUB(dist, vertices, via, startIterations[myLevel]));
+    }
+  }
+
+  return;
+}
+
+void HEARTBEAT_loop1_leftover(uint64_t *startIterations, uint64_t *maxIterations, void **liveInEnvironments, uint64_t myLevel) {
+  // load live-in environment
+  uint64_t *liveInEnvironment = ((uint64_t **)liveInEnvironments)[myLevel];
+  int *dist = (int *)liveInEnvironment[0];
+  int vertices = (int)liveInEnvironment[1];
+  int via = (int)liveInEnvironment[2];
+  int from = (int)liveInEnvironment[3];
+
+  for (; startIterations[myLevel] < maxIterations[myLevel]; startIterations[myLevel]++) {
+    if ((from != startIterations[myLevel]) && (from != via) && (startIterations[myLevel] != via)) {
+      SUB(dist, vertices, from, startIterations[myLevel]) =
+        std::min(SUB(dist, vertices, from, startIterations[myLevel]),
+                SUB(dist, vertices, from, via) + SUB(dist, vertices, via, startIterations[myLevel]));
+    }
+  }
+
+  return;
+}
+
 }
--- manual_transformation/benchmarks/floyd_warshall/loop_handler.hpp
+++ manual_transformation/benchmarks/floyd_warshall/loop_handler.hpp
@@ -104,7 +104,145 @@ int loop_handler(
   return 1;
 }
 #elif defined(HEARTBEAT_VERSIONING)
-  int loop_handler() { 
+int loop_handler(
+  uint64_t *startIterations,
+  uint64_t *maxIterations,
+  void **liveInEnvironments,
+  uint64_t myLevel,
+  void (*splittingTasks[])(uint64_t *, uint64_t *, void **, uint64_t),
+  void (*leftoverTasks[])(uint64_t *, uint64_t *, void **, uint64_t)
+) {
+  static std::atomic_bool *me = taskparts::hardware_alarm_polling_interrupt::my_heartbeat_flag();
+
+  // heartbeat doesn't happen
+  if (!(*me)) { 
     return 0;
   }
+
+  // heartbeat happens
+  (*me) = false;
+
+  // decide the splitting level
+  uint64_t splittingLevel = 1 + 1;
+  for (uint64_t level = 0; level <= myLevel; level++) {
+    if (maxIterations[level] - startIterations[level] >= 3) {
+      splittingLevel = level;
+      break;
+    }
+  }
+  // no more work to split at any level
+  if (splittingLevel > myLevel) {
+    return 0;
+  }
+
+  // allocate the new liveInEnvironments for both tasks
+  uint64_t *liveInEnvironmentsFirstHalf[2];
+  uint64_t *liveInEnvironmentsSecondHalf[2];
+  for (uint64_t level = 0; level <= splittingLevel; level++) {
+    liveInEnvironmentsFirstHalf[level] = ((uint64_t **)liveInEnvironments)[level];
+    liveInEnvironmentsSecondHalf[level] = ((uint64_t **)liveInEnvironments)[level];
+  }
+
+  // determine the splitting point of the remaining iterations
+  uint64_t med = (startIterations[splittingLevel] + 1 + maxIterations[splittingLevel]) / 2;
+
+  // allocate startIterations and maxIterations for both tasks
+  uint64_t startIterationsFirstHalf[2] =  { startIterations[0], startIterations[1]  };
+  uint64_t maxIterationsFirstHalf[2] =    { maxIterations[0],   maxIterations[1]    };
+  uint64_t startIterationsSecondHalf[2] = { startIterations[0], startIterations[1]  };
+  uint64_t maxIterationsSecondHalf[2] =   { maxIterations[0],   maxIterations[1]    };
+
+  // set startIterations and maxIterations for both tasks
+  startIterationsFirstHalf[splittingLevel]++;
+  maxIterationsFirstHalf[splittingLevel] = med;
+  startIterationsSecondHalf[splittingLevel] = med;
+
+  // when splittingLevel == myLevel, this eases things and won't be any leftover task
+  if (splittingLevel == myLevel) {
+#if defined(DEBUG_LOOP_HANDLER)
+    printf("Loop_handler: Start\n");
+    printf("Loop_handler:   Promotion\n");
+    printf("Loop_handler:   Receiving Level = %lu\n", myLevel);
+    printf("Loop_handler:   Splitting Level = %lu\n", splittingLevel);
+    printf("Loop_handler:   Current iteration state:\n");
+    for (uint64_t level = 0; level <= myLevel; level++) {
+      printf("Loop_handler:     level = %lu, startIteration = %lu, maxIteration = %lu\n", level, startIterations[level], maxIterations[level]);
+    }
+    printf("Loop_handler:     med = %lu\n", med);
+    printf("Loop_handler:     task1:      startIteration = %lu, maxIterations = %lu\n", startIterationsFirstHalf[myLevel], maxIterationsFirstHalf[myLevel]);
+    printf("Loop_handler:     task2:      startIteration = %lu, maxIterations = %lu\n", startIterationsSecondHalf[myLevel], maxIterationsSecondHalf[myLevel]);
+    printf("Loop_handler:     tail:\n");
+#endif
+    // reset maxIterations for the tail task
+    maxIterations[myLevel] = startIterations[myLevel] + 1;
+#if defined(DEBUG_LOOP_HANDLER)
+    printf("Loop_handler:       level = %lu, startIteration = %lu, maxIterations = %lu\n", level, startIterations[level], maxIterations[level]);
+#endif
+
+    taskparts::tpalrts_promote_via_nativefj([&] {
+      (*splittingTasks[myLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, myLevel);
+    }, [&] {
+      (*splittingTasks[myLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, myLevel);
+    }, [] { }, taskparts::bench_scheduler());
+  } else { // splittingLevel is higher than myLevel, build up the leftover task
+    // allocate the new liveInEnvironments for leftover task
+    uint64_t *liveInEnvironmentsLeftover[3];
+    for (uint64_t level = 0; level <= myLevel; level++) {
+      liveInEnvironmentsLeftover[level] = ((uint64_t **)liveInEnvironments)[level];
+    }
+
+    // allocate startIterations and maxIterations for leftover task
+    uint64_t startIterationsLeftover[2] =  { startIterations[0], startIterations[1]  };
+    uint64_t maxIterationsLeftover[2] =    { maxIterations[0],   maxIterations[1]    };
+
+    // set the startIterations for the leftover task
+    for (uint64_t level = splittingLevel + 1; level <= myLevel; level++) {
+      startIterationsLeftover[level] += 1;
+    }
+
+#if defined(DEBUG_LOOP_HANDLER)
+    printf("Loop_handler: Start\n");
+    printf("Loop_handler:   Promotion\n");
+    printf("Loop_handler:   Receiving Level = %lu\n", myLevel);
+    printf("Loop_handler:   Splitting Level = %lu\n", splittingLevel);
+    printf("Loop_handler:   Current iteration state:\n");
+    for (uint64_t level = 0; level <= myLevel; level++) {
+      printf("Loop_handler:     level = %lu, startIteration = %lu, maxIteration = %lu\n", level, startIterations[level], maxIterations[level]);
+    }
+    printf("Loop_handler:     med = %lu\n", med);
+    printf("Loop_handler:     task1:      startIteration = %lu, maxIterations = %lu\n", startIterationsFirstHalf[splittingLevel], maxIterationsFirstHalf[splittingLevel]);
+    printf("Loop_handler:     task2:      startIteration = %lu, maxIterations = %lu\n", startIterationsSecondHalf[splittingLevel], maxIterationsSecondHalf[splittingLevel]);
+    printf("Loop_handler:     leftover:\n");
+    for (uint64_t level = splittingLevel + 1; level <= myLevel; level++) {
+      printf("Loop_handler:       level = %lu, startIteration = %lu, maxIterations = %lu\n", level, startIterationsLeftover[level], maxIterationsLeftover[level]);
+    }
+    printf("Loop_handler:     tail:\n");
+#endif
+    // reset maxIterations for the tail task
+    for (uint64_t level = splittingLevel; level <= myLevel; level++) {
+      maxIterations[level] = startIterations[level] + 1;
+#if defined(DEBUG_LOOP_HANDLER)
+      printf("Loop_handler:       level = %lu, startIteration = %lu, maxIterations = %lu\n", level, startIterations[level], maxIterations[level]);
+#endif
+    }
+
+    // determine which leftover task to run
+    uint64_t leftoverTaskIndex = 0;
+    if (myLevel == 1 && (splittingLevel == 0 || splittingLevel == 1)) {
+      leftoverTaskIndex = 0;
+    }
+
+    taskparts::tpalrts_promote_via_nativefj([&] {
+      (*leftoverTasks[leftoverTaskIndex])(startIterationsLeftover, maxIterationsLeftover, (void **)liveInEnvironmentsLeftover, myLevel);
+    }, [&] {
+      taskparts::tpalrts_promote_via_nativefj([&] {
+        (*splittingTasks[splittingLevel])(startIterationsFirstHalf, maxIterationsFirstHalf, (void **)liveInEnvironmentsFirstHalf, splittingLevel);
+      }, [&] {
+        (*splittingTasks[splittingLevel])(startIterationsSecondHalf, maxIterationsSecondHalf, (void **)liveInEnvironmentsSecondHalf, splittingLevel);
+      }, [&] { }, taskparts::bench_scheduler());
+    }, [&] { }, taskparts::bench_scheduler());
+  }
+
+  return 1;
+}
 #endif
