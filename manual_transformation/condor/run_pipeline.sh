347fffaa0ed1365fc937cc20919ae51f5d27df88
Bangyen Pham <bangyen@northwestern.edu>
2022-10-26 22:09:56 -0500

    added environment variables

--- manual_transformation/condor/run_pipeline.sh
+++ manual_transformation/condor/run_pipeline.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
 cd /nfs-scratch/bhp1038/heart/manual_transformation/condor
-PROGRESS_FILE="progress.log"
-rm -f ${PROGRESS_FILE}
+FILE=progress.log
+OPENMP=OMP_NUM_THREADS
+HEARTB=TASKPARTS_NUM_WORKERS
+rm -f ${FILE}
 
-echo "### Pipeline Starts ###" > ${PROGRESS_FILE}
+echo "### Pipeline Starts ###" > ${FILE}
 
 # Setup environment for the heartbeat evaluation pipeline
-echo "### Setup Stage ###" >> ${PROGRESS_FILE}
-source ./bin/setup >> ${PROGRESS_FILE}
+echo "### Setup Stage ###" >> ${FILE}
+source ./bin/setup >> ${FILE}
 
 # Compile and collect metrics for the baseline
-echo "### Baseline Stage ###" >> ${PROGRESS_FILE}
-all "baseline" >> ${PROGRESS_FILE}
+echo "### Baseline Stage ###" >> ${FILE}
+all "baseline" >> ${FILE}
 
 # OpenCilk
-echo "### OpenCilk Stage ###" >> ${PROGRESS_FILE}
-all "opencilk" >> ${PROGRESS_FILE}
+echo "### OpenCilk Stage ###" >> ${FILE}
+all "opencilk" "CILK_NWORKERS" >> ${FILE}
 
 # OpenMP Static Scheduler
-echo "### OpenMP Static Stage ###" >> ${PROGRESS_FILE}
-all "openmp" >> ${PROGRESS_FILE}
+echo "### OpenMP Static Stage ###" >> ${FILE}
+all "openmp" $OPENMP >> ${FILE}
 
 # OpenMP Dynamic Scheduler
-echo "### OpenMP Dynamic Stage ###" >> ${PROGRESS_FILE}
-all "openmp_dynamic" >> ${PROGRESS_FILE}
+echo "### OpenMP Dynamic Stage ###" >> ${FILE}
+all "openmp_dynamic" $OPENMP >> ${FILE}
 
 # OpenMP Guided Scheduler
-echo "### OpenMP Guided Stage ###" >> ${PROGRESS_FILE}
-all "openmp_guided" >> ${PROGRESS_FILE}
+echo "### OpenMP Guided Stage ###" >> ${FILE}
+all "openmp_guided" $OPENMP >> ${FILE}
 
 # Compile and collect metrics for Heartbeat
-# echo "### Heartbeat Stage ###" >> ${PROGRESS_FILE}
-# ./bin/run_heartbeat >> ${PROGRESS_FILE}
+# echo "### Heartbeat Stage ###" >> ${FILE}
+# ./bin/run_heartbeat >> ${FILE}
 
-echo "### Pipeline Ends ###" >> ${PROGRESS_FILE}
+echo "### Pipeline Ends ###" >> ${FILE}
 exit 0
