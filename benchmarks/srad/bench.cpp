eb1fecb7c43987e2e1e93381ee1faa55785a8232
Yian Su <yiansu2018@u.northwestern.edu>
2023-03-07 16:54:01 -0600

    srad: fix input generator bug

--- benchmarks/srad/bench.cpp
+++ benchmarks/srad/bench.cpp
@@ -45,35 +45,13 @@ void run_bench(std::function<void()> const &bench_body,
 }
 #endif
 
-uint64_t hash64(uint64_t u) {
-  uint64_t v = u * 3935559000370003845ul + 2691343689449507681ul;
-  v ^= v >> 21;
-  v ^= v << 37;
-  v ^= v >>  4;
-  v *= 4768777513237032717ul;
-  v ^= v << 20;
-  v ^= v >> 41;
-  v ^= v <<  5;
-  return v;
-}
-
 void random_matrix(float *I, int rows, int cols){
-#if defined(INPUT_TPAL)
-  // tpal's input generator is falsy and leads to the final value in J to be -nan,
-  // however, to have a fair comparison against tpal, we keep this input generator
-  for( int i = 0 ; i < rows ; i++) {
-    for ( int j = 0 ; j < cols ; j++) {
-      I[i * cols + j] = (float)hash64(i+j)/(float)RAND_MAX ;
-    }
-  }
-#else
   srand(7);
 	for( int i = 0 ; i < rows ; i++){
 		for ( int j = 0 ; j < cols ; j++){
 		  I[i * cols + j] = rand()/(float)RAND_MAX ;
 		}
 	}
-#endif
 }
 
 void setup() {
